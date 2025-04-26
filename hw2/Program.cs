447ed2eeb8334fcf7a1de3db538c6df7d0eb90b7
Almog <almogto2@gmail.com>
2025-04-26 20:31:04 +0300

    Add PasswordHashingService registration in Program.cs

--- hw2/Program.cs
+++ hw2/Program.cs
@@ -1,8 +1,11 @@
+using hw2.Services; // Add this using statement
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSingleton<PasswordHashingService>(); // Register the service
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
